e97b0acd0da749ea245c86db190392a5049a4c34
Joshua Rollo <joshuamrollo@gmail.com>
2021-12-30 13:23:34 -0600

    final sorting visualizer project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import SortingMain from './SortingVisualizer/SortingMain';
+import {Helmet} from 'react-helmet'
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Sorting Visualizer</title>
+      </Helmet>
       <SortingMain />
     </div>
   );
